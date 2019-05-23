42aa8d87f662d559557552ccfd49594ad1118d82
Mathias Ferniss <5211966@RTS-365.dk>
2019-05-23 09:08:07 +0200

    header helt færdig mangler kun drobdown menu

--- script/Index.js
+++ script/Index.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     let nav = document.querySelector('.nav');
     let navwest = document.querySelector('#h1');
     let navhome = document.querySelector('.ul-nav');
+    let navblog = document.querySelector('#ul-nav');
     window.onscroll = function() {scrollFunction()};
     function scrollFunction() {
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
@@ -13,15 +14,18 @@ document.addEventListener('DOMContentLoaded', function(){
             navhome.style.color = "black";
             navhome.style.margintop = "0px";
             navhome.classList.remove(".nav__ul--color");
+            navblog.style.color = "black";
         } else {
  //         document.querySelector('nav').style.padding = "30px";
             nav.classList.remove("nav-scrolled");
             navwest.style.color = "white";
-            navwest.style.margintop = "";
+            navwest.style.margintop = "10px";
             navwest.classList.add("header__a");
             navhome.style.color = "white";
-            navhome.style.margintop = "";
+            navhome.style.margintop = "10px";
             navhome.classList.add(".nav__ul--color");
+            navblog.style.color = "white";
+            navblog.classList.add(".nav__ul--color");
         }
     }
 
